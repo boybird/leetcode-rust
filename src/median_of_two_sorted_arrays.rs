31d8f810e515078663dcf3107cd32a18c43e2b0a
zxk7516 <zxk7516@foxmail.com>
2019-03-08 08:14:00 +0800

    ..

---
+++ src/median_of_two_sorted_arrays.rs
@@ -0,0 +1,9 @@
+struct Solution {}
+
+
+impl Solution {
+    pub fn find_median_sorted_arrays(nums1: Vec<i32>, nums2: Vec<i32>) -> f64 {
+        let a = ((nums1.len()+nums2.len()) as f64)/2.0f64;
+        0.0f64        
+    }
+}
