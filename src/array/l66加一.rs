1ccd628490131fec94ba43b871551b248abb9d9d
zxk7516 <zxk7516@foxmail.com>
2020-03-18 18:09:42 +0800

    加一

--- src/array/l66加一.rs
+++ src/array/l66加一.rs
@@ -7,12 +7,32 @@ struct Solution;
 // @lc code=start
 impl Solution {
     pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
-        todo!()
+        let mut digits = digits;
+        let mut step = 1;
+        for n in digits.iter_mut().rev() {
+            *n = *n + step;
+            if *n >= 10 {
+                *n = *n - 10;
+                step = 1;
+            } else {
+                step = 0;
+                break;
+            }
+        }
+        if step > 0 {
+            digits.insert(0, step);
+        }
+        digits
     }
 }
 // @lc code=end
 
-
 mod tests {
-        
+
+    #[test]
+    fn test_it() {
+        use super::*;
+        dbg!(Solution::plus_one(vec![1, 2, 3]));
+        dbg!(Solution::plus_one(vec![4, 3, 2, 1]));
+    }
 }
